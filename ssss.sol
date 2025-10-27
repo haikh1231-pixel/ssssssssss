7d72b243dd2f3c5f96409857ad16ea456b65bd2a
Q-Idle User <user@qidle.dev>
2025-10-27 13:13:57 +0530

    added

---
+++ ssss.sol
@@ -0,0 +1,7 @@
+// Welcome to Q Remix IDE! 
+// Visit all Quranium websites at: https://quranium.org
+// Write your Solidity contract here...
+// pragma solidity ^0.8.7;
+// contract MyContract {
+// Your contract code goes here
+// }
